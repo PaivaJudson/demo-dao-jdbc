41a33902c962419b42f1196016c70c6b8343cf1c
judsonpaiva <judson.paiva@hotmail.com>
2022-04-19 20:04:56 +0100

    Creating project and git repository

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,14 +1,18 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department d1 = new Department(1, "Books");
+		Seller seller = new Seller(21, "Judson Paiva", "judson@gmail.com", new Date(), 3000.0, d1);
 		
-		System.out.println(d1);
+		System.out.println(seller);
 	}
 
 }
