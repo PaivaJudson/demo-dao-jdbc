eed86dbdbefe4e80d357f97f0c45cf135dbb5ac4
judsonpaiva <judson.paiva@hotmail.com>
2022-04-19 22:32:02 +0100

    SellerDaoJDBC e DaoFactory

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		
 		Department d1 = new Department(1, "Books");
 		Seller seller = new Seller(21, "Judson Paiva", "judson@gmail.com", new Date(), 3000.0, d1);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+	
+}
