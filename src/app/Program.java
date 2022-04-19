4cc6bdab71439872b37a37d0454cd41f4a725c48
judsonpaiva <judson.paiva@hotmail.com>
2022-04-19 19:50:01 +0100

    Department entity class

---
+++ src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department d1 = new Department(1, "Books");
+		
+		System.out.println(d1);
+	}
+
+}
